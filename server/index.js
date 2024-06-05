da96d2ab267ec22587b3d5aa491a45bad7c646d9
AEdwinGnanaOli <168707154+AEdwinGnanaOli@users.noreply.github.com>
2024-06-05 09:55:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const { MONGO_URL,PORT} = process.env;
 const app = express()
 app.use(
     cors({
-      origin: ["https://golden-nasturtium-2ddbc3.netlify.app/"],
+      origin: ["https://golden-nasturtium-2ddbc3.netlify.app"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
