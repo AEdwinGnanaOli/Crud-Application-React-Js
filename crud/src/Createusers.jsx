de5a36294888531a38def8b7abb2b9b614e9c2c1
AEdwinGnanaOli <edwina3942@gmail.com>
2024-07-04 09:13:55 +0530

    committed

--- crud/src/Createusers.jsx
+++ crud/src/Createusers.jsx
@@ -1,48 +1,100 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
-
-function CreateUser()
-{
-    const[name,setName] = useState("")
-    const[email,setEmail] = useState("")
-    const[age,setAge] = useState("")
-    const navigate = useNavigate()
-
-
-    const Submit = (e) => {
-        e.preventDefault();
-        axios.post("https://crud-application-react-js.onrender.com/create", {name,email,age})
-        .then(result =>{console.log(result)
-            navigate('/')
-        })
-        .catch(err =>console.log(err))
-    }
-    return(
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-        <div className="w-50 bg-white rounded p-3">
-            <form onSubmit={Submit}>
-                <h2>
-                    Add User
-                </h2>
-                <div className="mb-2">
-                   <label htmlFor="">Name</label>
-                    <input type="text" placeholder="Enter the name" className="form-control" onChange={(e)=>setName(e.target.value)}/>
-                </div>
-                <div className="mb-2">
-                   <label htmlFor="">Email</label>
-                    <input type="email" placeholder="Enter the email" className="form-control" onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
-                <div className="mb-2">
-                   <label htmlFor="">Age</label>
-                    <input type="number" placeholder="Enter age" className="form-control" onChange={(e)=>setAge(e.target.value)}/>
-                </div>
-                <button className="btn btn-success">Submit</button>
-            </form>
-
-        </div>
-        </div>
-    );
+import { useForm } from "react-hook-form";
+function CreateUser() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://crud-application-react-js.onrender.com/create", {
+        name,
+        email,
+        age,
+      })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <section id="user-crud-create">
+      <div className="create-user">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Add User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter the name"
+              className="form-control"
+              name="name"
+              {...register("name", {
+                required: "Name is Required",
+                validate: (value) => {
+                  if (value[0].trim() === value[0].toUpperCase()) {
+                    return true;
+                  }
+                  return "First letter is capital ";
+                },
+              })}
+              onChange={(e) => setName(e.target.value)}
+            />
+            {errors.name && (
+              <div className="text-danger">{errors.name.message}</div>
+            )}
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter the email"
+              className="form-control"
+              name="email"
+              {...register("email", {
+                required: "Email is required",
+                validate: (value) => {
+                  if (!value.includes("@")) {
+                    return "must includes in @";
+                  }
+                  return true;
+                },
+              })}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && (
+              <div className="text-danger">{errors.email.message}</div>
+            )}
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="number"
+              placeholder="Enter age"
+              className="form-control"
+              name="age"
+              {...register("age", {
+                required: "age number is required",
+              })}
+              onChange={(e) => setAge(e.target.value)}
+            />
+            {errors.age && (
+              <div className="text-danger">{errors.age.message}</div>
+            )}
+          </div>
+          <button className="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 export default CreateUser;
--- crud/src/Update.jsx
+++ crud/src/Update.jsx
@@ -1,64 +1,79 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-function UpdateUser()
-{
-    const {id} = useParams()
-    const[name,setName] = useState("")
-    const[email,setEmail] = useState("")
-    const[age,setAge] = useState("")
-    const navigate = useNavigate()
+function UpdateUser() {
+  const { id } = useParams();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        axios.get('https://crud-application-react-js.onrender.com/getUser/'+id)
-        .then(result => {console.log(result)
-            setName(result.data.name)
-            setEmail(result.data.email)
-            setAge(result.data.age)
-    })
-        .catch(err => console.log(err))
-    },[])
+  useEffect(() => {
+    axios
+      .get("https://crud-application-react-js.onrender.com/getUser/" + id)
+      .then((result) => {
+        console.log(result);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setAge(result.data.age);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-
-    const Update = (e) => {
-        e.preventDefault();
-        axios.put("https://crud-application-react-js.onrender.com/update/"+id, {name,email,age})
-        .then(result =>{console.log(result)
-            navigate('/')
-        })
-        .catch(err =>console.log(err))
-    }
-    return(
-        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
-        <div className="w-50 bg-white rounded p-3">
-            <form onSubmit={Update}>
-                <h2>
-                    update User
-                </h2>
-                <div className="mb-2">
-                   <label htmlFor="">Name</label>
-                    <input type="text" placeholder="Enter the name" className="form-control"
-                    value={name} onChange={(e)=>setName(e.target.value)}
-                    />
-                </div>
-                <div className="mb-2">
-                   <label htmlFor="">Email</label>
-                    <input type="email" placeholder="Enter the email" className="form-control"
-                    value={email} onChange={(e)=>setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="mb-2">
-                   <label htmlFor="">Age</label>
-                    <input type="number" placeholder="Enter age" className="form-control"
-                    value={age} onChange={(e)=>setAge(e.target.value)}
-                    />
-                </div>
-                <button className="btn btn-success">Update</button>
-            </form>
-
-        </div>
-        </div>
-    );
+  const Update = (e) => {
+    e.preventDefault();
+    axios
+      .put("https://crud-application-react-js.onrender.com/update/" + id, {
+        name,
+        email,
+        age,
+      })
+      .then((result) => {
+        console.log(result);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <section id="user-crud-create">
+      <div className="create-user">
+        <form onSubmit={Update}>
+          <h2>update User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter the name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter the email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="number"
+              placeholder="Enter age"
+              className="form-control"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-success">Update</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 export default UpdateUser;
